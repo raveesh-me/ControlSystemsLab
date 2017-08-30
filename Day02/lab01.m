08d042d31cff8c82d4fcae4b31b6f991bda916b7
Raveesh Agarwal <hi@raveeshagarwal.in>
2017-08-30 17:55:56 +0530

    clears the day03 confusion. Only two labs have taken place till date and updated lab02

--- Day02/lab01.m
+++ Day02/lab01.m
@@ -1,11 +1,17 @@
-%steady state position error in T0 and T1 systems
+% Declaring type 0 and type 1 systems
 n = [1];
-d1 = [1 3];
-sys1 = tf(n,d1)
-step(sys1)
-hold on;
+d0 = [1 3];
+sys0 = tf(n,d0)
 
-d2 = [1 2 1];
-sys2 = tf(n,d2)
-step(sys2)
+dref = [1]
+sysref = tf(n,dref)
+ramp(sysref)
 hold on
+%step(sys0)
+%ramp(sys0)
+
+n = [1]
+d1 = [1 2 1];
+sys1 = tf(n,d1)
+%step(sys1)
+ramp(sys1)
